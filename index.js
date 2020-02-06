88dac5ca6de8323af412f4b56e655207c1fd5a71
GusGo <gus.gonzalez.15@gmail.com>
2020-02-05 21:35:11 -0500

    created promt for inquirer

--- index.js
+++ index.js
@@ -1,14 +1,36 @@
 const inquirer = require('inquirer');
 
 const questions = [
-    'What is your GitHub username?',
-    ''
+    {
+        type: 'input',
+        message: 'What is your GitHub username?',
+        name: 'username'
+    },
+    {
+        type: 'list',
+        message: 'Pick a color:',
+        name: 'color',
+        choices: [
+            'green',
+            'blue',
+            'pink',
+            'red'
+        ]
+    }
 ];
 
 function writeToFile(fileName, data) {
 
 }
 
-function init() { }
+function init() {
+
+    // User prompt
+    inquirer.prompt(questions)
+        .then(function (response) {
+            console.log(response);
+            let username = response.username;
+        })
+}
 
 init();
